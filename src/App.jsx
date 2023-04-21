ac2821b8a75a5144b733c4c91cfff2a14aa3e875
Jeroen Leijnse <jr_leijnse@live.nl>
2023-04-21 20:09:35 +0200

    Starting screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import { useState } from "react";
+import BlueBlob from "./assets/blobblue.svg";
+import YellowBlob from "./assets/blobyellow.svg";
 
 export default function App() {
-  const [gameStarted, setGameStarted] = useState(true);
+  const [gameStarted, setGameStarted] = useState(false);
 
   return (
     <main>
-      {gameStarted ? <h1>Game has not started</h1> : <h1>Game has started</h1>}
+      {gameStarted ? (
+        <div className="flex-container">Questions</div>
+      ) : (
+        <div className="flex-container">
+          <h1 className="quiz-title">Quizzical</h1>
+          <span className="start-description">Some description if needed</span>
+          <button className="btn-start">Start Quiz</button>
+          <img className="blueblob" src={BlueBlob} />
+          <img className="yellowblob" src={YellowBlob} />
+        </div>
+      )}
     </main>
   );
 }
